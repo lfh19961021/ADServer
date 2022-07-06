aeb15a92924cea32307087bbf7f92cee067d3c47
Tony Fok <tony.wc.fok@hk.pwc.com>
2022-07-06 15:10:02 +0800

    store update

--- config/store.js
+++ config/store.js
@@ -1,6 +1,45 @@
+import MSSQLStore from 'connect-mssql-v2';
+import dbConfig from './db.js';
 
+/*
+ * Store Module
+ */
 export default {
-    loginMapping: [],
-    userMapping: [],
-    logoutMapping: []
-}
+	object: null,
+
+	get: {
+		config() {
+			return dbConfig[([process.env.NODE_ENV] == 'production' ? 'salesDashboard' : 'salesDashboardDev')];
+		},
+		options() {
+			return {
+				table: '[sessions]',
+				ttl: 1000 * 60 * 60 * 24 * 7,
+				autoRemove: true,
+				autoRemoveInterval: 1000 * 60 * 10,
+				autoRemoveCallback: () => {
+				},
+				useUTC: true,
+			};
+		},
+	},
+  
+	init() {
+		store.object = new MSSQLStore.default(store.get.config(), store.get.options());
+
+		store.object.on('connect', () => {
+			console.log('Session Store Connected');
+		});
+
+		store.object.on('error', (error) => {
+			console.log('Session Store Connected');
+			console.error(error);
+		});
+
+		store.object.on('sessionError', (error, classMethod) => {
+			console.log('Session Store Connected');
+			console.error(error);
+			console.error(classMethod);
+		});
+	},
+};
